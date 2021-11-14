53ea46725e4c379404b3f5ca7cbe5d8982a0d4aa
h3samayoa <h3samayoa@gmail.com>
2021-11-14 14:14:41 -0800

    added post route for user-routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -19,6 +19,42 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/:id', (req, res) => {
+    User.findOne({
+        attributes: {
+            exclude: ['password']
+        },
+        where: {
+            id: req.params.id,
+        },
+        include: [{
+            model: Post,
+            attributes: ['id', 'title', 'content', 'created_at']
+        },
+        {
+            model: Comment,
+            attributes: ['id', 'text', 'created_at'],
+            include: {
+                model: Post,
+                attributes: ['title']
+            }
+        }]
+    })
+    .then(dbUserData => {
+        if(!dbUserData) {
+            res.status(404).json({
+                message: 'No user found with that id'
+            });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+})
+
 router.post('/', (req, res) => {
     User.create({
         username: req.body.username,
