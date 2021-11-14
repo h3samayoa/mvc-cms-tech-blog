f8917d7c8614aa462b2104bb1c909739a308370d
h3samayoa <h3samayoa@gmail.com>
2021-11-14 14:06:11 -0800

    added comment routing

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -5,4 +5,26 @@ const {
     Comment
 } = require('../../models');
 
+router.get('/', (req, res) => {
+    Comment.findAll()
+        .then((dbCommentData) => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.post('/', (req, res) => {
+    Comment.create({
+        text: req.body.text,
+        post_id: req.body.post_id,
+        user_id: req.body.user_id,
+    })
+    .then((dbCommentData) => res.json(dbCommentData))
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    });
+});
+
 module.exports = router;
