e1c5b790afa4c28366bcb6a3d35892d5772d62e2
h3samayoa <h3samayoa@gmail.com>
2021-11-14 19:20:48 -0800

    added authentication to comment routes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -4,6 +4,7 @@ const {
     Post,
     Comment
 } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 router.get('/', (req, res) => {
     Comment.findAll()
@@ -14,17 +15,19 @@ router.get('/', (req, res) => {
         });
 });
 
-router.post('/', (req, res) => {
-    Comment.create({
-        text: req.body.text,
-        post_id: req.body.post_id,
-        user_id: req.body.user_id,
-    })
-    .then((dbCommentData) => res.json(dbCommentData))
-    .catch(err => {
-        console.log(err);
-        res.status(400).json(err);
-    });
+router.post('/', withAuth, (req, res) => {
+    if (req.session) {
+        Comment.create({
+            text: req.body.text,
+            post_id: req.body.post_id,
+            user_id: req.body.user_id,
+        })
+        .then((dbCommentData) => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    }
 });
 
 module.exports = router;
