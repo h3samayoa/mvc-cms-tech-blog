9fc26d19af3f55081489072d93bd2932518d18ce
h3samayoa <h3samayoa@gmail.com>
2021-11-14 19:48:14 -0800

    added add-post function

---
+++ public/js/add-post.js
@@ -0,0 +1,25 @@
+async function newPostHandler(event) {
+    event.preventDefault();
+
+    const title = document.querySelector('').value;
+    const post_content = document.querySelector('').value.trim();
+
+    const response = await fetch('/api/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+            title,
+            post_content,
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelector('').addEventListener('submit', newPostHandler);
